6fcc50ec8c4a39c1bd104fa4d7b3ba177dbe369a
uxelexx <i@kkornilov.ru>
2023-07-07 12:49:56 +0300

    feat(package): prettier/commitizen init

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+    singleQuote: true,
+    jsxSingleQuote: true,
+    arrowParens: 'avoid',
+    tabWidth: 2,
+    printWidth: 80,
+    semi: true,
+    trailingComma: 'all',
+};
