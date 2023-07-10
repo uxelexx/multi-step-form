307f0ecdd6e77753c8d820616c54e36135e9b070
uxelexx <i@kkornilov.ru>
2023-07-10 11:45:48 +0300

    feat(forms): plans form

---
+++ components/Forms/Plans.tsx
@@ -0,0 +1,71 @@
+import { useForm } from '@/context/form-context';
+import FormContainer from './FormContainer';
+import PlansOption from './PlansOption';
+import { ChangeEvent } from 'react';
+import PlansToggle from './PlansToggle';
+
+export default function Plan() {
+  const { form, setForm } = useForm();
+
+  function toggleYearly() {
+    setForm(prev => ({
+      ...prev,
+      plan: {
+        ...prev.plan,
+        yearly: !prev.plan.yearly,
+      },
+    }));
+  }
+
+  function handlePlan(e: ChangeEvent<HTMLInputElement>) {
+    setForm(prev => ({
+      ...prev,
+      plan: {
+        ...prev.plan,
+        type: e.target.value,
+      },
+    }));
+  }
+
+  return (
+    <FormContainer
+      heading='Select your plan'
+      description='You have the option of montly or yearly billing'
+    >
+      <div>
+        <div className='flex space-x-5'>
+          <PlansOption
+            yearly={form.plan.yearly}
+            onChange={handlePlan}
+            checked={form.plan.type === 'Arcade'}
+            name='arcade'
+            heading='Arcade'
+            price='9'
+            img='icon-arcade.svg'
+          />
+          <PlansOption
+            yearly={form.plan.yearly}
+            onChange={handlePlan}
+            checked={form.plan.type === 'Advanced'}
+            name='advanced'
+            heading='Advanced'
+            price='12'
+            img='icon-advanced.svg'
+          />
+          <PlansOption
+            yearly={form.plan.yearly}
+            onChange={handlePlan}
+            checked={form.plan.type === 'Pro'}
+            name='pro'
+            heading='Pro'
+            price='15'
+            img='icon-pro.svg'
+          />
+        </div>
+        <div className='pt-8'>
+          <PlansToggle onClick={toggleYearly} yearly={form.plan.yearly} />
+        </div>
+      </div>
+    </FormContainer>
+  );
+}
