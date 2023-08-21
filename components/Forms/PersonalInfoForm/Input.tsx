45992c6fe0dca316ba12e2647c27544e7d0a53cd
uxelexx <i@kkornilov.ru>
2023-08-21 14:17:25 +0300

    style(input): margin, text color

--- components/Forms/PersonalInfoForm/Input.tsx
+++ components/Forms/PersonalInfoForm/Input.tsx
@@ -6,11 +6,11 @@ type InputProps = InputHTMLAttributes<HTMLInputElement> & {
 
 export function Input({ label, ...rest }: InputProps) {
   return (
-    <label className='text-sm'>
+    <label className='text-indigo-950 capitalize text-sm'>
       {label}
       <input
         {...rest}
-        className='mt-1 font-medium placeholder:text-gray-200 border border-gray-300 rounded-xl p-3 w-full text-base outline-none focus:border-indigo-700'
+        className='mt-2 font-medium placeholder:text-gray-200 border border-gray-300 rounded-xl p-3 w-full text-base outline-none focus:border-indigo-700'
       />
     </label>
   );
