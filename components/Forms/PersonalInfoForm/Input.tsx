8a56c2cd652b8c7e1f7e3cf0df18be5cc3eaa6d3
uxelexx <i@kkornilov.ru>
2023-08-21 14:31:42 +0300

    refactor(input): add required field

--- components/Forms/PersonalInfoForm/Input.tsx
+++ components/Forms/PersonalInfoForm/Input.tsx
@@ -10,6 +10,7 @@ export function Input({ label, ...rest }: InputProps) {
       {label}
       <input
         {...rest}
+        required
         className='mt-2 font-medium placeholder:text-gray-200 border border-gray-300 rounded-xl p-3 w-full text-base outline-none focus:border-indigo-700'
       />
     </label>
