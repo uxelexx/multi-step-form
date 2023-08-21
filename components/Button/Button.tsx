cf914356720c6e2d356cff30f2e1a99a7f943f3b
uxelexx <i@kkornilov.ru>
2023-08-21 16:54:00 +0300

    feat(button): add variants

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -5,15 +5,28 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   label?: string;
   className?: string;
   onClick?: () => void;
+  variant?: keyof typeof variants;
 };
 
-export default function Button({ className, onClick, ...props }: ButtonProps) {
+const variants = {
+  primary: 'bg-indigo-800 hover:bg-indigo-600 text-white hover:bg-indigo-600',
+  secondary:
+    'bg-transparent hover:text-indigo-600 text-black text-opacity-30',
+};
+
+export default function Button({
+  className,
+  onClick,
+  variant = 'primary',
+  ...props
+}: ButtonProps) {
   return (
     <button
       {...props}
       onClick={onClick}
       className={cn(
-        'font-medium w-32 text-white bg-indigo-800 hover:bg-indigo-600 py-3 px-5 rounded-lg capitalize duration-75',
+        'font-medium w-32 py-3 px-5 rounded-lg capitalize duration-75',
+        variants[variant],
         className,
       )}
     >
