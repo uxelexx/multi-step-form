b85e505d9034520bdb80d4eeb4ab47f24497b167
uxelexx <i@kkornilov.ru>
2023-08-21 14:36:54 +0300

    refactor(sidebar): move step to separate component

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,20 +1,11 @@
-'use client';
-
 import { useForm } from '@/context/form-context';
 import SidebarStep from './SidebarStep';
-
-type SidebarProps = {
-  active: number;
-};
-
-const steps = ['Your info', 'Select plan', 'Add-ons', 'Summary'];
 
 export default function Sidebar() {
   const { form } = useForm();
 
   return (
     <div className='w-[29%] bg-sidebar space-y-6 rounded-xl p-9 bg-no-repeat'>
-      {steps.map((step, i) => {
         const isActive = form.formIndex === i;
         return (
           <SidebarStep
@@ -25,6 +16,5 @@ export default function Sidebar() {
           />
         );
       })}
-    </div>
   );
 }
