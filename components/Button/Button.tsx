a4215ba2fc9b8488570ca66c510809beee5f0f8f
uxelexx <i@kkornilov.ru>
2023-08-21 13:30:05 +0300

    refactor(button): props, cn merge fix

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import { cn } from '@/helpers/cn';
+import React, { ButtonHTMLAttributes } from 'react';
 
-type ButtonProps = {
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   label?: string;
   className?: string;
   onClick?: () => void;
 };
 
-export default function Button({ label, className, onClick }: ButtonProps) {
+export default function Button({
+  label,
+  className,
+  onClick,
+  ...props
+}: ButtonProps) {
   return (
     <button
+      {...props}
       onClick={onClick}
-      className={`font-medium w-32 text-white bg-indigo-800 hover:bg-indigo-600 py-3 px-5 rounded-lg capitalize duration-75 ${className}`}
+      className={cn(
+        'font-medium w-32 text-white bg-indigo-800 hover:bg-indigo-600 py-3 px-5 rounded-lg capitalize duration-75',
+        className,
+      )}
     >
       {label}
     </button>
