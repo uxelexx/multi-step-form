5476fc27fc3dd25ced488d0fa6f9dd349315fa68
uxelexx <i@kkornilov.ru>
2023-08-22 11:39:16 +0300

    feat(plan option): accessible radio buttons

--- components/Forms/PlansForm/PlanOption.tsx
+++ components/Forms/PlansForm/PlanOption.tsx
@@ -1,38 +1,73 @@
+import { cn } from '@/helpers/cn';
 import { formatPrice } from '@/helpers/formatPrice';
 import Image from 'next/image';
-import { InputHTMLAttributes } from 'react';
+import { useEffect, useRef } from 'react';
 
-type PlansOptionType = InputHTMLAttributes<HTMLInputElement> & {
+type PlansOptionType = {
   icon: string;
-  heading: string;
-  price: string;
+  price: number;
   yearly: boolean;
+  checked: boolean;
+  value: string;
+  onClick: (e: any) => void; // TODO: Add type
 };
 
 export default function PlanOption({
   icon,
-  heading,
   price,
   yearly,
-  ...props
+  checked,
+  value,
+  onClick,
 }: PlansOptionType) {
   const formatedPrice = formatPrice(yearly, price);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const radio = ref.current;
+
+    if (!radio) return;
+
+    function handleKeydown(e: KeyboardEvent) {
+      if (
+        document.activeElement === radio &&
+        (e.key === 'Enter' || e.key === ' ')
+      ) {
+        onClick(e);
+      }
+    }
+
+    radio.addEventListener('keydown', handleKeydown);
+
+    return () => {
+      radio.removeEventListener('keydown', handleKeydown);
+    };
+  }, [onClick]);
 
   return (
-    <label
-      className={`flex flex-col border border-gray-300 rounded-xl w-1/3 p-4 cursor-pointer hover:border-indigo-800 duration-75 ${
-        props.checked && 'bg-gray-100/50 border-indigo-800'
-      }`}
+    <div
+      role='radio'
+      ref={ref}
+      aria-checked={checked}
+      data-value={value}
+      onClick={onClick}
+      tabIndex={0}
+      className={cn(
+        'flex flex-col border border-gray-300 rounded-xl w-1/3 p-4 cursor-pointer hover:border-indigo-800 duration-75 outline-offset-4 outline-2 outline-indigo-300',
+        checked && 'bg-gray-100/50 border-indigo-800',
+      )}
     >
-      <input {...props} value={heading} type='radio' className='hidden' />
-      <Image src={icon} width={40} height={40} alt={heading} />
-      <div>
-        <h5 className='block font-medium text-[1.1rem] text-indigo-950 pt-12'>
-          {heading}
-        </h5>
-        <p className='text-[0.95rem] text-gray-400'>{formatedPrice}</p>
-        {yearly && <p className='text-indigo-950 text-sm'>2 months free</p>}
+      <label className='sr-only'>{value}</label>
+      <div className='pointer-events-none'>
+        <Image src={icon} width={40} height={40} alt={value} />
+        <div>
+          <h5 className='block font-medium text-[1.1rem] text-indigo-950 pt-12'>
+            {value}
+          </h5>
+          <p className='text-[0.95rem] text-gray-400'>{formatedPrice}</p>
+          {yearly && <p className='text-indigo-950 text-sm'>2 months free</p>}
+        </div>
       </div>
-    </label>
+    </div>
   );
 }
