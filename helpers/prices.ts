d591acec384fc24de67a4c1c5d0e7217ee9e58ae
uxelexx <i@kkornilov.ru>
2023-08-23 21:53:56 +0300

    refactor(prices): new fields in ADDS constant

--- helpers/prices.ts
+++ helpers/prices.ts
@@ -4,8 +4,21 @@ export const PLANS = {
   Pro: 15,
 } as const;
 
-export const ADDS_PRICE = {
-  service: 1,
-  storage: 2,
-  profile: 2,
+export const ADDS = {
+  service: {
+    label: 'Online service',
+    price: 1,
+    description: 'Access to multiplayer games',
+  },
+  storage: {
+    label: 'Extra storage',
+    price: 2,
+    description: 'Extra 1TB of cloud save',
+  },
+  profile: {
+    label: 'Customizable profile',
+    price: 2,
+    description: 'Custom theme on your profile',
+  },
 } as const;
+
