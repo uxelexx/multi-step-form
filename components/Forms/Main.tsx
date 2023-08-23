9753a3f084ddcbd3c7c64afe75ddce3786be5edf
uxelexx <i@kkornilov.ru>
2023-08-23 18:58:03 +0300

    refactor(main): remove unnecessary code

--- components/Forms/Main.tsx
+++ components/Forms/Main.tsx
@@ -4,28 +4,22 @@ import { useForm } from '@/context/form-context';
 import Sidebar from '../Sidebar/Sidebar';
 import AllForms, { FormIndex } from './AllForms';
 import FinalStep from './FinalStep';
-import MainLayout from './MainLayout';
 
 export default function Main() {
   const {
     form: { formIndex, done },
-    prevStep,
-    nextStep,
-    confirm,
   } = useForm();
 
   return (
-    <section className='flex content-center rounded-xl w-[min(58rem,90vw)] h-[37rem] bg-white p-4'>
+    <section className='flex rounded-xl w-[min(58rem,90vw)] h-[37rem] bg-white p-4'>
       <Sidebar />
-      <MainLayout
-        page={formIndex}
-        prevStep={prevStep}
-        nextStep={nextStep}
-        confirm={confirm}
-        done={done}
-      >
+      <div className='flex flex-col flex-1 pt-10 pb-5 px-20 h-full'>
         {done ? <FinalStep /> : <AllForms index={formIndex as FormIndex} />}
-      </MainLayout>
+      </div>
     </section>
   );
 }
+
+// <div className='flex flex-col flex-1 pt-10 pb-5 px-20 h-full'>
+//   {done ? <FinalStep /> : <AllForms index={formIndex as FormIndex} />}
+// </div>
