5495f82ab8fa860016706ccc0dabdd77dead3a3e
uxelexx <i@kkornilov.ru>
2023-08-23 20:29:34 +0300

    feat(prices): readonly constants

--- helpers/prices.ts
+++ helpers/prices.ts
@@ -1,11 +1,11 @@
-export const PLAN_PRICE: Record<string, number> = {
+export const PLAN_PRICE = {
   Arcade: 9,
   Advanced: 12,
   Pro: 15,
-};
+} as const;
 
 export const ADDS_PRICE = {
   service: 1,
   storage: 2,
   profile: 2,
-};
+} as const;
