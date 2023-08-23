057bf05408df41065cd96fbd3656b7e0c3b8e0f1
uxelexx <i@kkornilov.ru>
2023-08-23 21:11:32 +0300

    refactor(plans): toggle function, plans constant map

--- components/Forms/PlansForm/Plans.tsx
+++ components/Forms/PlansForm/Plans.tsx
@@ -1,18 +1,23 @@
 import { useForm } from '@/context/form-context';
+import { formatPrice } from '@/helpers/formatPrice';
+import { PLANS } from '@/helpers/prices';
 import { MouseEvent } from 'react';
 import FormContainer from '../FormContainer';
 import PlanOption from './PlanOption';
 import PlansToggle from './PlansToggle';
+import { toggleYearly } from '@/helpers/uitls';
 
 export default function Plan() {
-  const { form, setForm, toggleYearly } = useForm();
+  const { form, setForm } = useForm();
+
+  const toggle = () => toggleYearly(setForm);
 
   function handlePlan(e: MouseEvent<HTMLDivElement>) {
     const target = e.target as HTMLElement;
     setForm(prev => ({
       ...prev,
       plan:
-        (target.getAttribute('data-value') as keyof typeof PLAN_PRICE) ||
+        (target.getAttribute('data-value') as keyof typeof PLANS) ||
         'Arcade',
     }));
   }
@@ -23,8 +28,22 @@ export default function Plan() {
       description='You have the option of montly or yearly billing'
     >
       <fieldset role='radiogroup' className='flex space-x-5 mb-5'>
+        {Object.entries(PLANS).map(([plan, cost]) => {
+          const price = formatPrice(form.yearly, cost);
+
+          return (
+            <PlanOption
+              key={plan}
+              plan={plan}
+              price={price}
+              yearly={form.yearly}
+              checked={form.plan === plan}
+              onClick={handlePlan}
+            />
+          );
+        })}
       </fieldset>
-      <PlansToggle onClick={toggleYearly} yearly={form.plan.yearly} />
+      <PlansToggle onClick={toggle} yearly={form.yearly} />
     </FormContainer>
   );
 }
