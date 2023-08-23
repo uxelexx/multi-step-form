62899a34c8a02d1384f41ddb7835f642fe9c4ae6
uxelexx <i@kkornilov.ru>
2023-08-23 14:09:09 +0300

    refactor(plans): semantic tag, remove buttons

--- components/Forms/PlansForm/Plans.tsx
+++ components/Forms/PlansForm/Plans.tsx
@@ -1,14 +1,12 @@
-import Button from '@/components/Button/Button';
 import { useForm } from '@/context/form-context';
 import { MouseEvent } from 'react';
 import FormContainer from '../FormContainer';
 import PlanOption from './PlanOption';
 import PlansToggle from './PlansToggle';
 import plans from './plans.json';
-import { StepNav } from '@/components/Button/StepNav';
 
 export default function Plan() {
-  const { form, setForm, toggleYearly, nextStep, prevStep } = useForm();
+  const { form, setForm, toggleYearly } = useForm();
 
   function handlePlan(e: MouseEvent<HTMLDivElement>) {
     const target = e.target as HTMLElement;
@@ -26,7 +24,7 @@ export default function Plan() {
       heading='Select your plan'
       description='You have the option of montly or yearly billing'
     >
-      <form onSubmit={nextStep} className='h-full flex-col flex'>
+      <fieldset className='h-full flex-col flex space-y-5'>
         <div role='radiogroup' className='flex space-x-5'>
           {plans.map(option => (
             <PlanOption
@@ -38,13 +36,8 @@ export default function Plan() {
             />
           ))}
         </div>
-        <PlansToggle
-          className='mt-8'
-          onClick={toggleYearly}
-          yearly={form.plan.yearly}
-        />
-        <StepNav />
-      </form>
+        <PlansToggle onClick={toggleYearly} yearly={form.plan.yearly} />
+      </fieldset>
     </FormContainer>
   );
 }
