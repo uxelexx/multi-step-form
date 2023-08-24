afadb39410f749d5f86f787f1d58d61177be256f
uxelexx <i@kkornilov.ru>
2023-08-24 13:25:33 +0300

    feat(input): name prop type

--- components/Forms/PersonalInfoForm/Input.tsx
+++ components/Forms/PersonalInfoForm/Input.tsx
@@ -1,18 +1,20 @@
-import React, { InputHTMLAttributes } from 'react';
+import { PersonalInfoKeys } from "@/redux/features/personSlice";
+import React, { InputHTMLAttributes } from "react";
 
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
+  name: PersonalInfoKeys;
 };
 
-export function Input({ label, ...rest }: InputProps) {
+export function Input({ name, label, ...rest }: InputProps) {
   return (
-    <label className='text-indigo-950 capitalize text-sm'>
+    <label className="text-indigo-950 capitalize text-sm">
       {label}
       <input
+        name={name}
         {...rest}
-        required
-        className='mt-2 font-medium placeholder:text-gray-200 border border-gray-300 rounded-xl p-3 w-full text-base outline-none focus:border-indigo-700'
-        className='mt-2 font-medium placeholder:text-gray-200 border border-gray-300 rounded-xl p-3 w-full text-base focus:border-transparent'
+        // required ------------- UNCOMMENT LATER
+        className="mt-2 font-medium placeholder:text-gray-200 border border-gray-300 rounded-xl p-3 w-full text-base"
       />
     </label>
   );
