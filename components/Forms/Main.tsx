3814280468f717344df6ea6d47b7602851e23906
uxelexx <i@kkornilov.ru>
2023-08-24 14:08:35 +0300

    refactor(main): remove form context

--- components/Forms/Main.tsx
+++ components/Forms/Main.tsx
@@ -1,20 +1,18 @@
-'use client';
+"use client";
 
-import { useForm } from '@/context/form-context';
-import Sidebar from '../Sidebar/Sidebar';
-import AllForms, { FormIndex } from './AllForms';
-import FinalStep from './FinalStep';
+import { useAppSelector } from "@/redux/hooks";
+import Sidebar from "../Sidebar/Sidebar";
+import AllForms, { FormIndex } from "./AllForms";
+import FinalStep from "./FinalStep";
 
 export default function Main() {
-  const {
-    form: { formIndex, done },
-  } = useForm();
+  const { index, done } = useAppSelector((state) => state.formReducer);
 
   return (
-    <section className='flex rounded-xl w-[min(58rem,90vw)] h-[37rem] bg-white p-4'>
+    <section className="flex rounded-xl w-[min(58rem,90vw)] h-[37rem] bg-white p-4">
       <Sidebar />
-      <div className='flex flex-col flex-1 pt-10 pb-5 px-20 h-full'>
-        {done ? <FinalStep /> : <AllForms index={formIndex as FormIndex} />}
+      <div className="flex flex-col flex-1 pt-10 pb-5 px-20 h-full">
+        {done ? <FinalStep /> : <AllForms index={index as FormIndex} />}
       </div>
     </section>
   );
