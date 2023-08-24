31371cf97e7d381f852e1bb2c974eed7b38746a2
uxelexx <i@kkornilov.ru>
2023-08-24 13:43:25 +0300

    feat(store): add form reducer

--- redux/store.ts
+++ redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import personReducer from "./features/personSlice";
+import formReducer from "./features/formSlice";
 
 export const store = configureStore({
   reducer: {
     personReducer,
+    formReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
