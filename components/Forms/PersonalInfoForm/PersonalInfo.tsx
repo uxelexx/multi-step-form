7902c71f7c1575867ad47fd09bbc4f5070599f16
uxelexx <i@kkornilov.ru>
2023-08-24 13:26:09 +0300

    feat(personal info): change context to redux

--- components/Forms/PersonalInfoForm/PersonalInfo.tsx
+++ components/Forms/PersonalInfoForm/PersonalInfo.tsx
@@ -1,24 +1,25 @@
-import Button from '@/components/Button/Button';
-import { useForm } from '@/context/form-context';
-import { nextStep } from '@/helpers/uitls';
-import { ChangeEvent } from 'react';
-import FormContainer from '../FormContainer';
-import { Input } from './Input';
+import Button from "@/components/Button/Button";
+import { useForm } from "@/context/form-context";
+import { nextStep } from "@/helpers/uitls";
+import { ChangeEvent } from "react";
+import FormContainer from "../FormContainer";
+import { Input } from "./Input";
 
-// TODO:
-// 1) Refactor to useForm with Zod validation
+import { PersonalInfoKeys, updatePerson } from "@/redux/features/personSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 
 export default function PersonalInfo() {
-  const { form, setForm, lastPage } = useForm();
+  const { setForm, lastPage } = useForm();
+  const { userName, userEmail, userPhone } = useAppSelector(
+    (state) => state.personReducer,
+  );
+  const dispatch = useAppDispatch();
 
   function handleInputs(e: ChangeEvent<HTMLInputElement>) {
-    setForm(prev => ({
-      ...prev,
-      personal: {
-        ...prev.personal,
-        [e.target.name]: e.target.value,
-      },
-    }));
+    const name = e.target.name as PersonalInfoKeys;
+    const value = e.target.value;
+
+    dispatch(updatePerson({ name, value }));
   }
 
   function handleSubmit(e: ChangeEvent<HTMLFormElement>) {
@@ -28,39 +29,39 @@ export default function PersonalInfo() {
 
   return (
     <FormContainer
-      heading='Personal info'
-      description='Please provide your name, email address, and phone number.'
+      heading="Personal info"
+      description="Please provide your name, email address, and phone number."
     >
-      <form onSubmit={handleSubmit} className='flex flex-col h-full'>
-        <div className='flex flex-col space-y-6'>
+      <form onSubmit={handleSubmit} className="flex flex-col h-full">
+        <div className="flex flex-col space-y-6">
           <Input
             autoFocus
             min={5}
-            label='Name'
-            type='text'
-            name='userName'
-            placeholder='John Doe'
-            value={form.personal.userName}
+            label="Name"
+            type="text"
+            name="userName"
+            placeholder="John Doe"
+            value={userName}
             onChange={handleInputs}
           />
           <Input
-            label='Email address'
-            name='userEmail'
-            type='email'
-            placeholder='some@mail.com'
-            value={form.personal.userEmail}
+            label="Email address"
+            name="userEmail"
+            type="email"
+            placeholder="some@mail.com"
+            value={userEmail}
             onChange={handleInputs}
           />
           <Input
-            label='Phone number'
-            name='userPhone'
-            placeholder='+1 234 567 890'
-            type='tel'
-            value={form.personal.userPhone}
+            label="Phone number"
+            name="userPhone"
+            placeholder="+1 234 567 890"
+            type="tel"
+            value={userPhone}
             onChange={handleInputs}
           />
         </div>
-        <Button type='submit' className='mt-auto ml-auto'>
+        <Button type="submit" className="mt-auto ml-auto">
           Next step
         </Button>
       </form>
