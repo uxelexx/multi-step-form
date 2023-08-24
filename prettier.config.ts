570e2c905c5c04752649913f03a07ecc059dbcd7
uxelexx <i@kkornilov.ru>
2023-08-24 12:26:36 +0300

    feat(prettier config): type safe

---
+++ prettier.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "prettier";
+
+export default {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  arrowParens: "avoid",
+  tabWidth: 2,
+  printWidth: 80,
+  semi: true,
+  trailingComma: "all",
+} satisfies Config;
