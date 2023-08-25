d7d9a25a13f0abfa213f18eb5de3d54bd630c37f
uxelexx <i@kkornilov.ru>
2023-08-25 17:54:41 +0300

    feat(types): form  slice types

--- public/types/types.ts
+++ public/types/types.ts
@@ -1,5 +1,14 @@
-// Plan types
+// Form types
+export type FormSteps = "Your info" | "Select plan" | "Add-ons" | "Summary";
 
+export type FormInfo = {
+  index: number;
+  done: boolean;
+  yearly: boolean;
+  formSteps: FormSteps[];
+};
+
+// Plan types
 export type PlanPrices = {
   Arcade: number;
   Advanced: number;
