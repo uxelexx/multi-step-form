18fe04462f9f68ea3c527ec584c6fe3ef189a116
uxelexx <i@kkornilov.ru>
2023-08-25 14:52:21 +0300

    refactor(addons): remove form context, add addons slice

--- components/Forms/AddOns/AddOns.tsx
+++ components/Forms/AddOns/AddOns.tsx
@@ -1,40 +1,43 @@
-import { useForm } from '@/context/form-context';
-import { ADDS } from '@/helpers/prices';
-import { ChangeEvent } from 'react';
-import FormContainer from '../FormContainer';
-import AddOnsBox from './AddOnsCard';
+import { useForm } from "@/context/form-context";
+import { ChangeEvent } from "react";
+import FormContainer from "../FormContainer";
+import AddOnsBox from "./AddOnsCard";
 
-type AddsKeysType = keyof typeof ADDS;
+import { setAddons, type AddonsKeys } from "@/redux/features/addonsSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { formatPrice } from "@/helpers/formatPrice";
 
 export default function AddOns() {
-  const { form, setForm } = useForm();
+  const dispatch = useAppDispatch();
+  const addons = useAppSelector((state) => state.addonsReducer);
+  const { yearly } = useAppSelector((state) => state.formReducer);
 
   function handleAddOns(e: ChangeEvent<HTMLInputElement>) {
-    setForm(prev => ({
-      ...prev,
-      addOns: {
-        ...prev.addOns,
-        [e.target.name]: e.target.checked,
-      },
-    }));
+    dispatch(setAddons(e.target.name as AddonsKeys));
   }
 
   return (
     <FormContainer
-      heading='Pick add-ons'
-      description='Add-ons help enchance your gaming experience.'
+      heading="Pick add-ons"
+      description="Add-ons help enchance your gaming experience."
     >
-      <fieldset className='flex flex-col flex-1 space-y-5'>
-        {Object.entries(ADDS).map(([add, value]) => (
-          <AddOnsBox
-            key={add}
-            name={add}
-            checked={form.addOns[add as AddsKeysType].included}
-            onChange={handleAddOns}
-            yearly={form.yearly}
-            {...value}
-          />
-        ))}
+      <fieldset className="flex flex-col flex-1 space-y-5">
+        {Object.entries(addons).map(([add, value]) => {
+          const { label, description, price } = value;
+          const formatedPrice = formatPrice(yearly, price);
+
+          return (
+            <AddOnsBox
+              label={label}
+              key={add}
+              price={formatedPrice}
+              description={description}
+              name={add}
+              checked={addons[add as AddonsKeys].included}
+              onChange={handleAddOns}
+            />
+          );
+        })}
       </fieldset>
     </FormContainer>
   );
