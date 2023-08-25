5a65323fa0529f8a8abe764f9da0145bb76064a2
uxelexx <i@kkornilov.ru>
2023-08-25 18:40:01 +0300

    style(form container): formatting
    refactor(form container): remove optional in types

--- components/Forms/FormContainer.tsx
+++ components/Forms/FormContainer.tsx
@@ -1,10 +1,10 @@
-import React, { ReactNode } from 'react';
-import { StepNav } from '../Button/StepNav';
+import React, { type ReactNode } from "react";
+import { StepNav } from "../Button/StepNav";
 
 type FormContainerProps = {
-  heading?: string;
-  description?: string;
-  children?: ReactNode;
+  heading: string;
+  description: string;
+  children: ReactNode;
 };
 
 export default function FormContainer({
@@ -13,9 +13,9 @@ export default function FormContainer({
   children,
 }: FormContainerProps) {
   return (
-    <div className='flex flex-col h-full'>
-      <h2 className='font-bold pb-2 text-4xl text-indigo-950'>{heading}</h2>
-      <p className='text-gray-400 pb-10'>{description}</p>
+    <div className="flex flex-col h-full">
+      <h2 className="font-bold pb-2 text-4xl text-indigo-950">{heading}</h2>
+      <p className="text-gray-400 pb-10">{description}</p>
       {children}
       <StepNav />
     </div>
