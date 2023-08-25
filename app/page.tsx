f38a3050435187e5705df9b400d645899a269a12
uxelexx <i@kkornilov.ru>
2023-08-25 17:15:05 +0300

    feat(form context): remove form context

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import Main from '@/components/Forms/Main';
-import { FormProvider } from '@/context/form-context';
+import Main from "@/components/Forms/Main";
 
 export default function Home() {
   return (
-    <main className='grid place-items-center min-h-screen font-ubuntu'>
-      <FormProvider>
-        <Main />
-      </FormProvider>
+    <main className="grid place-items-center min-h-screen font-ubuntu">
+      <Main />
     </main>
   );
 }
