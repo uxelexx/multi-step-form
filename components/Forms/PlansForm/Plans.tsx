ca411bda6b102b4112585a055aa40745c49f0528
uxelexx <i@kkornilov.ru>
2023-08-25 14:29:17 +0300

    refactor(plans): remove context, add plan slice

--- components/Forms/PlansForm/Plans.tsx
+++ components/Forms/PlansForm/Plans.tsx
@@ -1,48 +1,53 @@
-import { useForm } from '@/context/form-context';
-import { formatPrice } from '@/helpers/formatPrice';
-import { PLANS } from '@/helpers/prices';
-import { MouseEvent } from 'react';
-import FormContainer from '../FormContainer';
-import PlanOption from './PlanOption';
-import PlansToggle from './PlansToggle';
-import { toggleYearly } from '@/helpers/uitls';
+import { formatPrice } from "@/helpers/formatPrice";
+import { type MouseEvent } from "react";
+import FormContainer from "../FormContainer";
+import PlanOption from "./PlanOption";
+import PlansToggle from "./PlansToggle";
+
+import { toggleYearly } from "@/redux/features/formSlice";
+import {
+  plansPrices,
+  setPlan,
+  type PlansKey,
+} from "@/redux/features/planSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 
 export default function Plan() {
-  const { form, setForm } = useForm();
+  const { yearly } = useAppSelector((state) => state.formReducer);
+  const plan = useAppSelector((state) => state.planReducer);
+  const dispatch = useAppDispatch();
 
-  const toggle = () => toggleYearly(setForm);
+  const toggleYear = () => dispatch(toggleYearly());
 
   function handlePlan(e: MouseEvent<HTMLDivElement>) {
     const target = e.target as HTMLElement;
-    setForm(prev => ({
-      ...prev,
-      plan:
-        (target.getAttribute('data-value') as keyof typeof PLANS) || 'Arcade',
-    }));
+    const plan = target.getAttribute("data-value") as PlansKey;
+
+    dispatch(setPlan(plan));
   }
 
   return (
     <FormContainer
-      heading='Select your plan'
-      description='You have the option of montly or yearly billing'
+      heading="Select your plan"
+      description="You have the option of montly or yearly billing"
     >
-      <fieldset role='radiogroup' className='flex space-x-5 mb-5'>
-        {Object.entries(PLANS).map(([plan, cost]) => {
-          const price = formatPrice(form.yearly, cost);
+      <fieldset role="radiogroup" className="flex space-x-5 mb-5">
+        {Object.entries(plansPrices).map(([planType, cost]) => {
+          const price = formatPrice(yearly, cost);
 
           return (
             <PlanOption
-              key={plan}
-              plan={plan}
+              key={planType}
+              plan={planType}
               price={price}
-              yearly={form.yearly}
-              checked={form.plan === plan}
+              yearly={yearly}
+              checked={plan.type === planType}
               onClick={handlePlan}
             />
           );
         })}
       </fieldset>
-      <PlansToggle onClick={toggle} yearly={form.yearly} />
+      <PlansToggle onClick={toggleYear} yearly={yearly} />
     </FormContainer>
   );
 }
