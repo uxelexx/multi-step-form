aa2fb7582e6f41fe503d48661fefffb2ffa33b1c
uxelexx <i@kkornilov.ru>
2023-08-25 14:53:39 +0300

    refactor(addons card): remove unused props

--- components/Forms/AddOns/AddOnsCard.tsx
+++ components/Forms/AddOns/AddOnsCard.tsx
@@ -1,22 +1,18 @@
-import { cn } from '@/helpers/cn';
-import { formatPrice } from '@/helpers/formatPrice';
-import { InputHTMLAttributes, useRef } from 'react';
-import { AddOnsCheckboxInput } from './AddOnsCheckboxInput';
+import { cn } from "@/helpers/cn";
+import { InputHTMLAttributes, useRef } from "react";
+import { AddOnsCheckboxInput } from "./AddOnsCheckboxInput";
 
 type AddOnsBoxProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
   description: string;
-  price: number;
-  yearly: boolean;
+  price: string;
 };
 export default function AddOnsBox({
   label,
   description,
   price,
-  yearly,
   ...props
 }: AddOnsBoxProps) {
-  const formatedPrice = formatPrice(yearly, price);
   const inputRef = useRef<HTMLInputElement>(null);
 
   function handleDivClick() {
@@ -27,18 +23,18 @@ export default function AddOnsBox({
     <div
       onClick={handleDivClick}
       className={cn(
-        'flex relative items-center text-indigo-950 outline-indigo-300 outline-offset-3 border hover:border-indigo-950 border-gray-300 rounded-xl py-4 px-7 cursor-pointer space-x-6',
-        props.checked && 'border-indigo-800 bg-indigo-100/50',
+        "flex relative items-center text-indigo-950 outline-indigo-300 outline-offset-3 border hover:border-indigo-950 border-gray-300 rounded-xl py-4 px-7 cursor-pointer space-x-6",
+        props.checked && "border-indigo-800 bg-indigo-100/50",
       )}
     >
       <AddOnsCheckboxInput ref={inputRef} {...props} />
-      <div className='flex-1 pointer-events-none'>
-        <label className='font-medium leading-5 text-[1.05rem] block'>
+      <div className="flex-1 pointer-events-none">
+        <label className="font-medium leading-5 text-[1.05rem] block">
           {label}
         </label>
-        <span className='text-gray-400'>{description}</span>
+        <span className="text-gray-400">{description}</span>
       </div>
-      <p className='text-indigo-600'>{formatedPrice}</p>
+      <p className="text-indigo-600">{price}</p>
     </div>
   );
 }
