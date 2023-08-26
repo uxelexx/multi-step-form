21f3e4850d7f104e9ea3045d85e9f8bb6f8f9ac5
uxelexx <i@kkornilov.ru>
2023-08-26 13:27:01 +0300

    style(button): formatting

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,5 +1,5 @@
-import { cn } from '@/helpers/cn';
-import React, { ButtonHTMLAttributes } from 'react';
+import { cn } from "@/helpers/cn";
+import { ButtonHTMLAttributes } from "react";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   label?: string;
@@ -9,14 +9,14 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
 };
 
 const variants = {
-  primary: 'bg-indigo-800 hover:bg-indigo-600 text-white hover:bg-indigo-600',
-  secondary: 'bg-transparent hover:text-indigo-600 text-black text-opacity-30',
+  primary: "bg-indigo-800 hover:bg-indigo-600 text-white hover:bg-indigo-600",
+  secondary: "bg-transparent hover:text-indigo-600 text-black text-opacity-30",
 };
 
 export default function Button({
   className,
   onClick,
-  variant = 'primary',
+  variant = "primary",
   ...props
 }: ButtonProps) {
   return (
@@ -24,7 +24,7 @@ export default function Button({
       {...props}
       onClick={onClick}
       className={cn(
-        'font-medium w-32 py-3 px-5 rounded-lg capitalize duration-75',
+        "font-medium w-32 py-3 px-5 rounded-lg capitalize duration-75",
         variants[variant],
         className,
       )}
