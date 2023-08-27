0b94de7871050f38d2894f48f42e8f70d333dc5a
uxelexx <i@kkornilov.ru>
2023-08-27 18:08:01 +0300

    refactor(input): rename props

--- components/Forms/PersonalInfoForm/Input.tsx
+++ components/Forms/PersonalInfoForm/Input.tsx
@@ -6,14 +6,13 @@ type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   name: PersonalInfoField;
 };
 
-export function Input({ name, label, ...rest }: InputProps) {
+export function Input({ name, label, ...props }: InputProps) {
   return (
     <label className="text-indigo-950 capitalize text-sm">
       {label}
       <input
         name={name}
-        {...rest}
-        // required ------------- UNCOMMENT LATER
+        {...props}
         className="mt-2 font-medium placeholder:text-gray-200 border border-gray-300 rounded-xl p-3 w-full text-base"
       />
     </label>
