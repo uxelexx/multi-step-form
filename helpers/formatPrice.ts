59ac88634929419e8307726b5c293500bf2f894f
uxelexx <i@kkornilov.ru>
2023-08-27 18:05:51 +0300

    refactor(format price): remove type check

--- helpers/formatPrice.ts
+++ helpers/formatPrice.ts
@@ -1,5 +1,3 @@
-export function formatPrice(yearly: boolean, price: number | string) {
-  if (Number.isNaN(Number(price))) throw Error('wrong price format');
-
-  return yearly ? `$${Number(price) * 10}/yr` : `$${Number(price)}/mo`;
+export function formatPrice(yearly: boolean, price: number) {
+  return yearly ? `$${price * 10}/yr` : `$${price}/mo`;
 }
